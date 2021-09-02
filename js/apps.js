be07caac346d6133aaebac1b65fb1189d0ac0ab1
Rjrazu <razuahmed00042@gmail.com>
2021-09-02 21:50:36 +0600

    final update

--- js/apps.js
+++ js/apps.js
@@ -37,7 +37,7 @@ const displaySearchResult = books => {
     const searchResult = document.getElementById('search-result');
     searchResult.textContent = '';
     books.forEach(book => {
-
+        console.log(book)
         // Dynamic image link
         const urlForImage = `https://covers.openlibrary.org/b/id/${book.cover_i}.jpg`;
 
@@ -45,13 +45,14 @@ const displaySearchResult = books => {
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
         div.innerHTML = `
-            <div class="card shadow border-1 rounded-3" style="width: 20rem;">
+            <div class="card shadow border-1 rounded-3" style="width: 21rem;">
                   <img class="w-100 p-3 border rounded-3" src="${urlForImage}"  alt="COver Page Of Book">
                <div class="card-body"> 
                      <h5 class="card-title">Title of Book : ${book.title}</h5>
                      <h6 class="card-text">Author : ${book.author_name}</h6>
+                     <h6 class="card-text">Publisher : ${book.publisher}</h6>
                      <p class="card-text">First Publish Year : ${book.first_publish_year}</p>
-                     <a target="_blank" href='${urlForImage}' class="btn btn-outline-warning">Click Here For Full Cover Page!</a>
+                     <a target="_blank" href='${urlForImage}' class="btn btn-outline-warning">See Full Cover Photo!</a>
                 </div>
             </div>
         `;
